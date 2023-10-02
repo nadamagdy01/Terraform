7e6eb8205b8efa749b0e8cf39948e84e5e6e7f40
nadamagdy01 <140888715+nadamagdy01@users.noreply.github.com>
2023-10-02 14:51:06 +0300

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "iti-s3bucket-tfstate" # iti-s3bucket-tfstate is the name of the S3 I created manually
+    key    = "~/Documents/Terraform/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "state_lock" # state_lock is the name of the dynamobd I created manually
+  }
+}
